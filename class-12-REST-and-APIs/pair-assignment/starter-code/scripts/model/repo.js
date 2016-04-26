a45c80d23960e008f83884e4abcad360dcb72c48
MatthewAllanWilson <mattwils@gmail.com>
2016-04-26 12:46:08 -0700

    added ajax to requestRepos function

--- class-12-REST-and-APIs/pair-assignment/starter-code/scripts/model/repo.js
+++ class-12-REST-and-APIs/pair-assignment/starter-code/scripts/model/repo.js
@@ -4,8 +4,17 @@
   repos.all = [];
 
   repos.requestRepos = function(callback) {
-    // TODO: How would you like to fetch your repos? Someone say AJAX!?
+    // DONE!!: How would you like to fetch your repos? Someone say AJAX!?
     //  Don't forget to call the callback!
+    $.ajax({
+      url: 'https://api.github.com/users/' + githubUser + '/repos' + '?per_page=10' + '&sort=updated',
+      type: 'GET',
+      headers: {'Authorization': 'token ' + githubToken},
+      success: function(data) {
+        console.log(data);
+        callback();
+      }
+    });
 
   };
 
